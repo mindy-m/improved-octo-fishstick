6e3f409fac00cf51e9dee47ce8d7d5103b9d844f
Mindy Medeiros <mmedei6@wgu.edu>
2023-12-17 12:14:37 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::io::stdin;
+
+fn main() {
+    let mut player_hp = 50;
+    let mut opponent_hp = 100;
+    let mut potion_count = 3;
+
+    println!("Welcome!  You're in a boss fight!  Isn't that exciting...\n");
+    while player_hp > 0 && opponent_hp > 0 {
+        println!(
+            "Your current hp is {player_hp} and your opponent's hp is {}",
+            opponent_hp
+        );
+        println!("Would you like to attack or use a potion?");
+        let input_line = match stdin().lines().next() {
+            Some(woot) => woot,
+            None => {
+                println!("Well I guess you're done with the game. Bye!");
+                return;
+            }
+        };
+        let input_line = match input_line {
+            Ok(yay) => yay,
+            Err(uhoh) => {
+                println!("There was an error! {uhoh}");
+                return;
+            }
+        };
+        let mut player_move = false;
+        if input_line == "attack" {
+            opponent_hp -= 10;
+            println!("You have dealt 10 hp damage to your opponent.");
+            player_move = true;
+        } else if input_line == "potion" {
+            if potion_count > 0 {
+                player_hp += 30;
+                println!("You drank a potion. You healed 30 hp.");
+                if player_hp > 50 {
+                    player_hp = 50;
+                    println!("You are fully healed, but you wasted some of your potion.");
+                }
+                potion_count -= 1;
+                player_move = true;
+            } else if potion_count == 0 {
+                println!("Sorry, you're out of potions.");
+            } else {
+                println!("Something went wackadoodle.");
+            }
+        } else {
+            println!("Please choose either 'attack' or 'potion'.");
+        }
+        if player_move {
+            player_hp -= 10;
+            println!("Your opponent dealt 10 hp damage.\n");
+        }
+    }
+    if player_hp == 0 {
+        if opponent_hp == 0 {
+            println!(
+                "While you defeated your opponent, you are also dead, so you can't celebrate."
+            );
+        }
+        println!("Game over, you lost.");
+    } else if opponent_hp == 0 {
+        println!("Congratulations, you won!");
+    } else {
+        println!("I don't know what happened.");
+    }
+}
+
+/*
+std::io::stdin() (gives us a Stdin)
+  .lines() (gives us a Lines???)
+  .next() (gives us Option<Result<String, _>>)
+  .unwrap() (gives us Result<String, _>)
+  .unwrap() (gives us String)
+  yay a String! <---
+
+Option<T> = there might be a T, there might be no T
+Result<T, E> = we might succeed and give you a T, we might fail and give you an E
+*/
